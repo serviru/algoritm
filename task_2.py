3a9687aecdd1e9a1d83c27e6aa0bec9e7cfd1d9a
serviru <46635487+serviru@users.noreply.github.com>
2021-10-03 13:13:00 +0500

    практическая работа 4
    Это сложно но возможно, кажется что начинаю понимать

--- task_2.py
+++ task_2.py
@@ -1,21 +1,90 @@
 """
 Задание 2.
 
-Реализуйте два алгоритма.
+Приведен код, который формирует из введенного числа
+обратное по порядку входящих в него цифр.
+Задача решена через рекурсию
+Выполнена попытка оптимизировать решение через мемоизацию.
+Сделаны замеры обеих реализаций.
 
-Первый, в виде функции, должен обеспечивать поиск минимального значения для списка.
-В основе алгоритма должно быть сравнение каждого числа со всеми другими элементами списка.
-Сложность такого алгоритма: O(n^2) - квадратичная.
-
-Второй, в виде функции, должен обеспечивать поиск минимального значения для списка.
-Сложность такого алгоритма: O(n) - линейная.
-
-Не забудьте указать сложность каждого из двух алгоритмов. Для лучшего закрепления темы
-можете определить и указать сложность еще и у каждого выражения этих двух алгоритмов.
-
-Примечание:
-Прошу вас внимательно читать ТЗ и не забыть выполнить все пункты.
-
-Постарайтесь не использовать ф-ции min() и sort() и другие ф-ции!
-Подход должен быть максимально алгоритмическим.
+Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?!!!
+Будьте внимательны, задание хитрое. Не все так просто, как кажется.
 """
+
+from timeit import timeit
+from random import randint
+
+
+def recursive_reverse(number):
+    if number == 0:
+        return ''
+    return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+
+num_100 = randint(10000, 1000000)
+num_1000 = randint(1000000, 10000000)
+num_10000 = randint(100000000, 10000000000000)
+
+print('Не оптимизированная функция recursive_reverse')
+print(
+    timeit(
+        "recursive_reverse(num_100)",
+        setup='from __main__ import recursive_reverse, num_100',
+        number=10000))
+print(
+    timeit(
+        "recursive_reverse(num_1000)",
+        setup='from __main__ import recursive_reverse, num_1000',
+        number=10000))
+print(
+    timeit(
+        "recursive_reverse(num_10000)",
+        setup='from __main__ import recursive_reverse, num_10000',
+        number=10000))
+
+
+def memoize(f):
+    cache = {}
+
+    def decorate(*args):
+
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = f(*args)
+            return cache[args]
+    return decorate
+
+
+@memoize
+def recursive_reverse_mem(number):
+    if number == 0:
+        return ''
+    return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
+
+
+print('Оптимизированная функция recursive_reverse_mem')
+print(
+    timeit(
+        'recursive_reverse_mem(num_100)',
+        setup='from __main__ import recursive_reverse_mem, num_100',
+        number=10000))
+print(
+    timeit(
+        'recursive_reverse_mem(num_1000)',
+        setup='from __main__ import recursive_reverse_mem, num_1000',
+        number=10000))
+print(
+    timeit(
+        'recursive_reverse_mem(num_10000)',
+        setup='from __main__ import recursive_reverse_mem, num_10000',
+        number=10000))
+
+'''
+Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?!!!
+Будьте внимательны, задание хитрое. Не все так просто, как кажется.
+
+Данный примет явно показать преимущества и недостатки не может. По причинам 
+того что мемоизация, нет повторений привыполнении цикла,  не корректные условия примения данного примера. Если как  
+нестандартное решение то вероятно применима. А так нет. Нельзя сравнивать
+'''

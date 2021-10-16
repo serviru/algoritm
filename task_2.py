3fa42350a8f76cd5caf0b300b46e482df0ec7b2f
serviru <46635487+serviru@users.noreply.github.com>
2021-10-16 21:06:00 +0500

    практическая  работа 8
    копипаст, второе задание не делал

--- task_2.py
+++ task_2.py
@@ -1,21 +1,79 @@
 """
 Задание 2.
 
-Реализуйте два алгоритма.
+Доработайте пример структуры "дерево",
+рассмотренный на уроке.
 
-Первый, в виде функции, должен обеспечивать поиск минимального значения для списка.
-В основе алгоритма должно быть сравнение каждого числа со всеми другими элементами списка.
-Сложность такого алгоритма: O(n^2) - квадратичная.
+Предложите варианты доработки и оптимизации
+(например, валидация значений узлов в соответствии с требованиями для бинарного дерева).
 
-Второй, в виде функции, должен обеспечивать поиск минимального значения для списка.
-Сложность такого алгоритма: O(n) - линейная.
-
-Не забудьте указать сложность каждого из двух алгоритмов. Для лучшего закрепления темы
-можете определить и указать сложность еще и у каждого выражения этих двух алгоритмов.
-
-Примечание:
-Прошу вас внимательно читать ТЗ и не забыть выполнить все пункты.
-
-Постарайтесь не использовать ф-ции min() и sort() и другие ф-ции!
-Подход должен быть максимально алгоритмическим.
+Поработайте с доработанной структурой, позапускайте на реальных данных - на клиентском коде.
 """
+
+class BinaryTree:
+    def __init__(self, root_obj):
+        # корень
+        self.root = root_obj
+        # левый потомок
+        self.left_child = None
+        # правый потомок
+        self.right_child = None
+
+    # добавить левого потомка
+    def insert_left(self, new_node):
+        # если у узла нет левого потомка
+        if self.left_child == None:
+            # тогда узел просто вставляется в дерево
+            # формируется новое поддерево
+            self.left_child = BinaryTree(new_node)
+        # если у узла есть левый потомок
+        else:
+            # тогда вставляем новый узел
+            tree_obj = BinaryTree(new_node)
+            # и спускаем имеющегося потомка на один уровень ниже
+            tree_obj.left_child = self.left_child
+            self.left_child = tree_obj
+
+    # добавить правого потомка
+    def insert_right(self, new_node):
+        # если у узла нет правого потомка
+        if self.right_child == None:
+            # тогда узел просто вставляется в дерево
+            # формируется новое поддерево
+            self.right_child = BinaryTree(new_node)
+        # если у узла есть правый потомок
+        else:
+            # тогда вставляем новый узел
+            tree_obj = BinaryTree(new_node)
+            # и спускаем имеющегося потомка на один уровень ниже
+            tree_obj.right_child = self.right_child
+            self.right_child = tree_obj
+
+    # метод доступа к правому потомку
+    def get_right_child(self):
+        return self.right_child
+
+    # метод доступа к левому потомку
+    def get_left_child(self):
+        return self.left_child
+
+    # метод установки корня
+    def set_root_val(self, obj):
+        self.root = obj
+
+    # метод доступа к корню
+    def get_root_val(self):
+        return self.root
+
+
+r = BinaryTree(8)
+print(r.get_root_val())
+print(r.get_left_child())
+r.insert_left(40)
+print(r.get_left_child())
+print(r.get_left_child().get_root_val())
+r.insert_right(12)
+print(r.get_right_child())
+print(r.get_right_child().get_root_val())
+r.get_right_child().set_root_val(16)
+print(r.get_right_child().get_root_val())
